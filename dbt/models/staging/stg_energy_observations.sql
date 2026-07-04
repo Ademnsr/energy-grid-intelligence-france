e3673ea6b7b8ff8960cacd069267c53d27b60978
Adem Nasraoui <medademnasraoui10@gmail.com>
2026-07-04 21:34:49 +0200

    Phase 2: modeles staging dbt (meteo + consommation) sous dbt/

---
+++ dbt/models/staging/stg_energy_observations.sql
@@ -0,0 +1,7 @@
+select
+    id,
+    period_type,
+    start_date,
+    end_date,
+    consumption_mw
+from raw.energy_observations
---
+++ dbt/models/staging/stg_weather_observations.sql
@@ -0,0 +1,9 @@
+select
+    id,
+    latitude,
+    longitude,
+    observed_at,
+    temperature_2m,
+    relative_humidity_2m,
+    wind_speed_10m
+from raw.weather_observations
